b3f6be2000bca3060a07e4f12a91b87dcbb3d560
SchadenKai <kairustecsonpersonal@gmail.com>
2024-09-04 20:59:34 +0800

    feat: added deployment folders for deploying web and api separately

--- backend/scripts/setup_environment.ps1
+++ backend/scripts/setup_environment.ps1
@@ -1,4 +1,4 @@
-cd ..
+Set-Location ..
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 . .venv\Scripts\Activate.ps1
 pip install -r ConversationalHealthPlatform/backend/requirements/default.txt
@@ -6,4 +6,4 @@ pip install -r ConversationalHealthPlatform/backend/requirements/dev.txt
 pip install -r ConversationalHealthPlatform/backend/requirements/model_server.txt
 pip install -r ConversationalHealthPlatform/backend/requirements/ee.txt
 pip install -r ConversationalHealthPlatform/backend/requirements/cdk.txt
-cd ConversationalHealthPlatform
+Set-Location ConversationalHealthPlatform
