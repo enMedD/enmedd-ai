97037f282afeb31f1b5fb5a743c5f263b466c2ca
ervinpiol <rvnpiol14@gmail.com>
2024-09-22 11:20:26 +0800

    feat: change delete button to hoverable delete

--- web/src/components/DeleteButton.tsx
+++ web/src/components/DeleteButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "./ui/button";
+/* import { Button } from "./ui/button";
 import { Trash } from "lucide-react";
 
 export function DeleteButton({
@@ -13,5 +13,33 @@ export function DeleteButton({
       <Trash className="mr-1 my-auto" size={16} />
       Delete
     </Button>
+  );
+} */
+import { CustomTooltip } from "./CustomTooltip";
+import { Button } from "./ui/button";
+import { Trash } from "lucide-react";
+
+export function DeleteButton({
+  onClick,
+  disabled,
+}: {
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void | Promise<void>;
+  disabled?: boolean;
+}) {
+  return (
+    <CustomTooltip
+      trigger={
+        <Button
+          onClick={onClick}
+          variant="ghost"
+          size="icon"
+          disabled={disabled}
+        >
+          <Trash className="shrink-0" size={16} />
+        </Button>
+      }
+    >
+      Delete
+    </CustomTooltip>
   );
 }
