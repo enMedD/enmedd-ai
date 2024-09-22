09d67b41f66d078d1eea7f6023d6d03bb7117aba
ervinpiol <rvnpiol14@gmail.com>
2024-09-22 11:21:34 +0800

    feat: add asChild prop to delete button

--- web/src/components/DeleteButton.tsx
+++ web/src/components/DeleteButton.tsx
@@ -1,20 +1,3 @@
-/* import { Button } from "./ui/button";
-import { Trash } from "lucide-react";
-
-export function DeleteButton({
-  onClick,
-  disabled,
-}: {
-  onClick?: (event: React.MouseEvent<HTMLElement>) => void | Promise<void>;
-  disabled?: boolean;
-}) {
-  return (
-    <Button onClick={onClick} variant="destructive" disabled={disabled}>
-      <Trash className="mr-1 my-auto" size={16} />
-      Delete
-    </Button>
-  );
-} */
 import { CustomTooltip } from "./CustomTooltip";
 import { Button } from "./ui/button";
 import { Trash } from "lucide-react";
@@ -38,6 +21,7 @@ export function DeleteButton({
           <Trash className="shrink-0" size={16} />
         </Button>
       }
+      asChild
     >
       Delete
     </CustomTooltip>
