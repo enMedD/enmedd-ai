29d77a3f393b141aee3848a926348537b13e8981
Amboyandrey <andreyamboy13@gmail.com>
2024-10-22 14:50:29 +0800

    refractor: remove curators on backend

--- backend/enmedd/server/manage/users.py
+++ backend/enmedd/server/manage/users.py
@@ -26,7 +26,6 @@ from sqlalchemy.orm import Session
 
 from ee.enmedd.db.api_key import is_api_key_email_address
 from ee.enmedd.db.external_perm import delete_user__ext_teamspace_for_user__no_commit
-from ee.enmedd.db.teamspace import remove_curator_status__no_commit
 from ee.enmedd.server.workspace.store import _PROFILE_FILENAME
 from ee.enmedd.server.workspace.store import upload_profile
 from enmedd.auth.invited_users import get_invited_users
@@ -34,7 +33,6 @@ from enmedd.auth.invited_users import write_invited_users
 from enmedd.auth.noauth_user import fetch_no_auth_user
 from enmedd.auth.noauth_user import set_no_auth_user_preferences
 from enmedd.auth.schemas import ChangePassword
-from enmedd.auth.schemas import UserRole
 from enmedd.auth.schemas import UserStatus
 from enmedd.auth.users import current_admin_user
 from enmedd.auth.users import current_teamspace_admin_user
@@ -176,12 +174,6 @@ def set_user_role(
     if not user_to_update:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if user_role_update_request.new_role == UserRole.CURATOR:
-        raise HTTPException(
-            status_code=400,
-            detail="Curator role must be set via the User Group Menu",
-        )
-
     if user_to_update.role == user_role_update_request.new_role:
         return
 
@@ -190,9 +182,6 @@ def set_user_role(
             status_code=400,
             detail="An admin cannot demote themselves from admin role!",
         )
-
-    if user_to_update.role == UserRole.CURATOR:
-        remove_curator_status__no_commit(db_session, user_to_update)
 
     user_to_update.role = user_role_update_request.new_role.value
 
