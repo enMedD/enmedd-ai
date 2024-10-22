1967540cca83d47387c7eec67d3c07bfaab644e0
SchadenKai <kairustecsonpersonal@gmail.com>
2024-10-22 14:06:04 +0800

    fix: creating document set not working

--- web/src/components/UserProfile.tsx
+++ web/src/components/UserProfile.tsx
@@ -181,6 +181,9 @@ interface UserProfileProps {
 
 const getNameInitials = (fullName: string) => {
   const names = fullName.split(" ");
+  console.log(
+    names[0][0].toUpperCase() + names[names.length - 1][0].toUpperCase()
+  );
   return names[0][0].toUpperCase() + names[names.length - 1][0].toUpperCase();
 };
 
