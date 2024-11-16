91919e1c5c94b539c06687edc94d1b05990931e0
ervinpiol <rvnpiol14@gmail.com>
2024-11-16 11:09:08 +0800

    save

--- web/src/components/admin/Layout.tsx
+++ web/src/components/admin/Layout.tsx
@@ -1,3 +1,73 @@
+// import { User, UserRole } from "@/lib/types";
+// import {
+//   AuthTypeMetadata,
+//   getAuthTypeMetadataSS,
+//   getCurrentTeamspaceUserSS,
+//   getCurrentUserSS,
+// } from "@/lib/userSS";
+// import { redirect } from "next/navigation";
+// import { SideBar } from "../SideBar";
+// import { BarLayout } from "../BarLayout";
+// import { AdminBar } from "./AdminBar";
+// import { HealthCheckBanner } from "../health/healthcheck";
+
+// export async function Layout({
+//   children,
+//   isTeamspace,
+//   teamspaceId,
+// }: {
+//   children: React.ReactNode;
+//   isTeamspace?: boolean;
+//   teamspaceId?: string;
+// }) {
+//   const tasks = [
+//     getAuthTypeMetadataSS(),
+//     getCurrentUserSS(),
+//     teamspaceId ? getCurrentTeamspaceUserSS(teamspaceId) : null,
+//   ];
+
+//   // catch cases where the backend is completely unreachable here
+//   // without try / catch, will just raise an exception and the page
+//   // will not render
+//   let results: (User | AuthTypeMetadata | null)[] = [null, null];
+//   try {
+//     results = await Promise.all(tasks);
+//   } catch (e) {
+//     console.log(`Some fetch failed for the main search page - ${e}`);
+//   }
+
+//   const authTypeMetadata = results[0] as AuthTypeMetadata | null;
+//   const user = teamspaceId
+//     ? (results[2] as User | null)
+//     : (results[1] as User | null);
+
+//   const authDisabled = authTypeMetadata?.authType === "disabled";
+//   const requiresVerification = authTypeMetadata?.requiresVerification;
+
+//   if (!authDisabled) {
+//     if (!user) {
+//       return redirect("/auth/login");
+//     }
+//     if (user.role === UserRole.BASIC) {
+//       return redirect("/");
+//     }
+//     if (!user.is_verified && requiresVerification) {
+//       return redirect("/auth/waiting-on-verification");
+//     }
+//   }
+
+//   return (
+//     <div className="h-full">
+//       <HealthCheckBanner />
+//       <div className="flex h-full">
+//         <AdminBar user={user}>
+//           <SideBar isTeamspace={isTeamspace} />
+//         </AdminBar>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// }
 import { User, UserRole } from "@/lib/types";
 import {
   AuthTypeMetadata,
@@ -10,6 +80,9 @@ import { SideBar } from "../SideBar";
 import { BarLayout } from "../BarLayout";
 import { AdminBar } from "./AdminBar";
 import { HealthCheckBanner } from "../health/healthcheck";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { MainSidebar } from "./MainSidebar";
+import { Separator } from "../ui/separator";
 
 export async function Layout({
   children,
@@ -57,14 +130,21 @@ export async function Layout({
   }
 
   return (
-    <div className="h-full">
-      <HealthCheckBanner />
-      <div className="flex h-full">
-        <AdminBar user={user}>
-          <SideBar isTeamspace={isTeamspace} />
-        </AdminBar>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+      className="h-full w-full"
+    >
+      <MainSidebar user={user} />
+      <SidebarInset className="w-full overflow-hidden">
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4 absolute top-0">
+          <SidebarTrigger className="-ml-1" />
+        </header>
         {children}
-      </div>
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
---
+++ web/src/components/admin/MainSidebar.tsx
@@ -0,0 +1,208 @@
+"use client";
+
+import { useContext } from "react";
+import ArnoldAi from "../../../public/arnold_ai.png";
+
+import { Label } from "@/components/ui/label";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarInput,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Switch } from "@/components/ui/switch";
+import { GlobalSidebar } from "./GlobalSidebar";
+import Image from "next/image";
+import { CustomTooltip } from "../CustomTooltip";
+import Link from "next/link";
+import { Logo } from "../Logo";
+import { SettingsContext } from "../settings/SettingsProvider";
+import { useParams } from "next/navigation";
+import { User } from "@/lib/types";
+import { buildImgUrl } from "@/app/chat/files/images/utils";
+import { ArchiveX, Ellipsis, File, Inbox, Send, Trash2 } from "lucide-react";
+import { AdminSidebar } from "./AdminSidebar";
+
+export function MainSidebar({
+  user,
+  ...props
+}: React.ComponentProps<typeof Sidebar> & { user?: User | null }) {
+  const { teamspaceId } = useParams();
+  const { setOpen } = useSidebar();
+  const combinedSettings = useContext(SettingsContext);
+  if (!combinedSettings) {
+    return null;
+  }
+  const settings = combinedSettings.settings;
+  const workspaces = combinedSettings.workspaces;
+  const defaultPage = settings.default_page;
+
+  let teamsapces = user?.groups || [];
+  if (teamspaceId) {
+    const matchingTeamspace = teamsapces.find(
+      (group) => group.id.toString() === teamspaceId
+    );
+    const otherTeamspaces = teamsapces.filter(
+      (group) => group.id.toString() !== teamspaceId
+    );
+    teamsapces = matchingTeamspace
+      ? [matchingTeamspace, ...otherTeamspaces]
+      : otherTeamspaces;
+  }
+  const displayedTeamspaces = teamsapces.slice(0, 8);
+  const showEllipsis = user?.groups && user.groups.length > 8;
+
+  const generateGradient = (teamspaceName: string) => {
+    const colors = ["#CCCCCC", "#999999", "#666666", "#333333", "#000000"];
+    const index = teamspaceName.charCodeAt(0) % colors.length;
+    return `linear-gradient(135deg, ${colors[index]}, ${colors[(index + 1) % colors.length]})`;
+  };
+
+  return (
+    <Sidebar
+      collapsible="icon"
+      className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row"
+      {...props}
+    >
+      {/* This is the first sidebar */}
+      {/* We disable collapsible and adjust width to icon. */}
+      {/* This will make the sidebar appear as icons. */}
+      <Sidebar
+        collapsible="none"
+        className="!w-[calc(var(--sidebar-width-icon)_+_1px)] border-r"
+      >
+        <SidebarHeader className="">
+          <SidebarMenu>
+            <SidebarGroupContent className="space-y-2.5">
+              <SidebarMenuItem className="border-b pb-2.5">
+                <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
+                  <a href="#">
+                    <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                      <Image
+                        src={ArnoldAi}
+                        alt="Arnold AI Logo"
+                        width={32}
+                        height={32}
+                        className="rounded-regular shrink-0"
+                      />
+                    </div>
+                    {/* <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">Acme Inc</span>
+                      <span className="truncate text-xs">Enterprise</span>
+                    </div> */}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              <SidebarMenuItem className="border-b pb-2.5">
+                <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
+                  <CustomTooltip
+                    trigger={
+                      <Link
+                        href={`/${defaultPage}`}
+                        className="flex items-center justify-center"
+                      >
+                        {workspaces?.use_custom_logo ? (
+                          <Logo />
+                        ) : (
+                          <Image
+                            src={ArnoldAi}
+                            alt="Arnold AI Logo"
+                            width={32}
+                            height={32}
+                          />
+                        )}
+                      </Link>
+                    }
+                    side="right"
+                    delayDuration={0}
+                    asChild
+                  >
+                    {workspaces?.workspace_name
+                      ? workspaces.workspace_name
+                      : "Arnold AI"}
+                  </CustomTooltip>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarGroupContent>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent className="px-1.5 md:px-0">
+              <SidebarMenu className="space-y-1">
+                {displayedTeamspaces.map((teamspace) => (
+                  <SidebarMenuItem key={teamspace.id}>
+                    <SidebarMenuButton
+                      tooltip={{
+                        children: teamspace.name,
+                        hidden: false,
+                      }}
+                      className={`!p-0 ${Number(teamspaceId) === teamspace.id ? "bg-secondary-500" : ""}`}
+                      isActive={teamspace.id.toString() === teamspaceId}
+                    >
+                      <Link
+                        href={`/t/${teamspace.id}/${defaultPage}`}
+                        className="w-full h-full flex items-center justify-center"
+                      >
+                        {teamspace.logo ? (
+                          <img
+                            src={buildImgUrl(teamspace.logo)}
+                            alt="Teamspace Logo"
+                            // className={`object-cover shrink-0 ${Number(teamspaceId) === teamspace.id ? "w-7 h-7 rounded-[8px]" : "w-full h-full"}`}
+                            className={`object-cover shrink-0 ${Number(teamspaceId) === teamspace.id ? "h-7 w-7 rounded-sm" : "w-9 h-full"}`}
+                            width={28}
+                            height={28}
+                          />
+                        ) : (
+                          <div
+                            style={{
+                              background: generateGradient(teamspace.name),
+                            }}
+                            // className={`font-bold text-inverted shrink-0  bg-brand-500 flex justify-center items-center uppercase ${Number(teamspaceId) === teamspace.id ? "w-7 h-7 rounded-[8px]" : "w-full h-full"}`}
+                            className={`font-bold text-inverted shrink-0  bg-brand-500 flex justify-center items-center uppercase ${Number(teamspaceId) === teamspace.id ? "h-7 w-8 rounded-sm" : "w-full h-full"}`}
+                          >
+                            {teamspace.name.charAt(0)}
+                          </div>
+                        )}
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+
+                {!showEllipsis && (
+                  <SidebarMenuItem>
+                    <SidebarMenuButton
+                      tooltip={{
+                        children: "More",
+                        hidden: false,
+                      }}
+                      className="p-0 hover:bg-light hover:text-accent-foreground focus-visible:ring-light flex items-center justify-center"
+                    >
+                      <Ellipsis size={16} />
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                )}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <GlobalSidebar user={user} />
+        </SidebarFooter>
+      </Sidebar>
+
+      {/* This is the second sidebar */}
+      {/* We disable collapsible and let it fill remaining space */}
+
+      <AdminSidebar />
+    </Sidebar>
+  );
+}
