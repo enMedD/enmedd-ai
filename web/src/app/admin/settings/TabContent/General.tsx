31d0f6cb65f7e91341bb0018cad7f0462388c832
Ervin Piol <112147440+ervinpiol@users.noreply.github.com>
2024-11-21 16:10:39 +0800

    Feature/rebranding/new frontend branch (#263)
    * feat: change endpoint of invite and remove user
    
    * fix: font inter
    
    * feat: fix cancel invite modal
    
    * feat: change the user profile on shared chat
    
    * feat: add secondary theme color
    
    * chore: remove comment on tailwind
    
    * feat: make useGradient hook
    
    * feat: change the FileUpload to ImageUpload component
    
    * fix: empty assistant input
    
    * fix: unchecked of checkbox
    
    * feat: add color picker on theme change
    
    * feat: remove usage statistics on teamspace
    
    * feat: remove the
    
    * fix: query not working on teamspace
    
    * fix: do the necessary fix
    
    * feat: add cursor
    
    * feat: change leader email to name
    
    * chore: clean comments
    
    * feat: add secondary theme changer (#259)
    
    * feat: change endpoint of invite and remove user
    
    * fix: font inter
    
    * feat: fix cancel invite modal
    
    * feat: change the user profile on shared chat
    
    * feat: add secondary theme color
    
    * chore: remove comment on tailwind
    
    * feat: make useGradient hook
    
    * feat: change the FileUpload to ImageUpload component
    
    * fix: empty assistant input
    
    ---------
    
    Co-authored-by: Kai Tecson <111247289+SchadenKai@users.noreply.github.com>
    
    * Feature/workspace creation (#260)
    
    * feat: new endpoints for workspace management
    
    * fix: delete teamsapce; cascade all
    
    * feat: enhance teamspace creation with document set and credential pair checks
    
    * fix: search route
    
    ---------
    
    Co-authored-by: Kai Tecson <111247289+SchadenKai@users.noreply.github.com>
    Co-authored-by: Niño Andrey Amboy <104948678+Amboyandrey@users.noreply.github.com>
    Co-authored-by: Amboyandrey <andreyamboy13@gmail.com>
    Co-authored-by: SchadenKai <kairustecsonpersonal@gmail.com>

--- web/src/app/admin/settings/TabContent/General.tsx
+++ web/src/app/admin/settings/TabContent/General.tsx
@@ -75,6 +75,35 @@ export default function General() {
     fetchThemes();
   }, []);
 
+  const [primaryColor, setPrimaryColor] = useState("#65007E");
+  const [secondaryColor, setSecondaryColor] = useState("#EEB3FE");
+
+  useEffect(() => {
+    const fetchThemes = async () => {
+      try {
+        const response = await fetch("/api/themes", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const themes = await response.json();
+
+        setPrimaryColor(themes.brand["500"]);
+        setSecondaryColor(themes.secondary["500"]);
+      } catch (error) {
+        console.error("Error fetching themes:", error);
+      }
+    };
+
+    fetchThemes();
+  }, []);
+
   async function updateWorkspaces(newValues: Workspaces) {
     const response = await fetch("/api/admin/workspace", {
       method: "PUT",
