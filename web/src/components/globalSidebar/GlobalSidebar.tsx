982deb58c8be73102806abd65c4d543a550b2a23
ervinpiol <rvnpiol14@gmail.com>
2024-12-04 09:47:38 +0800

    feat: change basic user alert when no data source

--- web/src/components/globalSidebar/GlobalSidebar.tsx
+++ web/src/components/globalSidebar/GlobalSidebar.tsx
@@ -191,7 +191,7 @@ export function GlobalSidebar({
             </SidebarGroupContent>
           </SidebarGroup>
         </SidebarContent>
-        <SidebarFooter className="!py-2.5">
+        <SidebarFooter>
           <UserSettingsButton defaultPage={defaultPage} />
         </SidebarFooter>
       </Sidebar>
