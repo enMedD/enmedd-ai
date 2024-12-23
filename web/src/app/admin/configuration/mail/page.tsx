6599154a1929709b45da68684ecf3d9e5b6fe74b
HueHueberry <huby.new1112@gmail.com>
2024-12-23 21:41:47 +0800

    feat: implemented debounce search

--- web/src/app/admin/configuration/mail/page.tsx
+++ web/src/app/admin/configuration/mail/page.tsx
@@ -9,34 +9,65 @@ import { getEmailTemplates } from "../../../../lib/email_templates";
 
 import { useEffect, useState } from "react";
 import { EmailTemplates } from "@/lib/types";
+import { Loading } from "@/components/Loading";
+import { SearchInput } from "@/components/SearchInput";
 
 function Main() {
   const [openMailTemplateModal, setOpenMailTemplateModal] = useState(false);
   const [forceRefresh, setForceRefresh] = useState(0);
   const [templateList, setTempplateList] = useState<EmailTemplates[]>([]);
   const [selectedTemplate, setSelectedTemplate] = useState<EmailTemplates>();
+  const [retrieveLoading, setRetrieveLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     (async () => {
+      setRetrieveLoading(true);
       const response = await getEmailTemplates();
+
       setTempplateList(response);
+      setRetrieveLoading(false);
     })();
   }, [forceRefresh]);
 
+  // debounce trigger when searching
+  useEffect(() => {
+    if (searchValue !== "") {
+      const timer = setTimeout(() => {
+        (async () => {
+          setRetrieveLoading(true);
+          const response = await getEmailTemplates();
+          setTempplateList(
+            response.filter((mailTmp) =>
+              mailTmp.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+          );
+          setRetrieveLoading(false);
+        })();
+      }, 500);
+
+      return () => clearTimeout(timer);
+    } else {
+      setForceRefresh(forceRefresh + 1);
+    }
+  }, [searchValue]);
+
   const onMailTemplateUpdate = () => {
     setSelectedTemplate(undefined);
-    setForceRefresh(forceRefresh + 1)
-  }
+    setForceRefresh(forceRefresh + 1);
+  };
 
   return (
     <>
-      {selectedTemplate && <MailTemplateModal
-        open={openMailTemplateModal}
-        templateData={selectedTemplate}
-        setOpen={setOpenMailTemplateModal}
-        onUpdate={onMailTemplateUpdate}
-        onClose={() => setSelectedTemplate(undefined)}
-      />}
+      {selectedTemplate && (
+        <MailTemplateModal
+          open={openMailTemplateModal}
+          templateData={selectedTemplate}
+          setOpen={setOpenMailTemplateModal}
+          onUpdate={onMailTemplateUpdate}
+          onClose={() => setSelectedTemplate(undefined)}
+        />
+      )}
       <h4 className="pb-2 mb-4 text-xl font-bold text-text-800 text-text">
         Customize Your Email Templates
       </h4>
@@ -47,7 +78,21 @@ function Main() {
         invitations are clear and engaging.
       </p>
       <br />
+      <SearchInput
+        placeholder="Search Email Template"
+        value={searchValue}
+        onChange={(value) => {
+          setSearchValue(value);
+        }}
+      />
+      <br />
       <div className="relative w-full flex gap-2 flex-wrap">
+        {retrieveLoading && (
+          <div className="flex justify-center items-center gap-1">
+            <Loading />
+            <>{" Retrieving Templates..."}</>
+          </div>
+        )}
         {templateList.map((template) => (
           <TemplateCard
             title={template.title}
