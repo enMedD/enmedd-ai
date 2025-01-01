0e2c47453555e16c76d8366a546968717c345217
ervinpiol <rvnpiol14@gmail.com>
2025-01-01 11:32:50 +0800

    feat: add basic user description

--- web/src/app/chat/NoValidAssistantModal.tsx
+++ web/src/app/chat/NoValidAssistantModal.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { useState } from "react";
 import { CustomModal } from "@/components/CustomModal";
 import { Assistant } from "../admin/assistants/interfaces";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { User } from "@/lib/types";
-import { useTeamspaces, useTeamspaceUsers } from "@/lib/hooks";
 
 export function NoValidAssistantModal({
   assistants,
@@ -17,15 +14,21 @@ export function NoValidAssistantModal({
   teamspaceId: string | undefined;
   isTeamspaceAdmin: boolean;
 }) {
-  const { data } = useTeamspaces();
+  const basicUserDescription = `
+    Currently, there is no valid assistant assigned to this teamspace. This means the chat feature is unavailable for now. 
+    The teamspace may still be syncing, or no assistant has been assigned yet. Please check back later, or reach out to the teamspace administrator for more details.
+  `;
+
+  const adminDescription = `
+    There is currently no valid assistant assigned to this teamspace, so the chat page cannot be used. This may be because the teamspace is still syncing, or an assistant has not been assigned yet. As an administrator, you can visit the admin panel to review the teamspace status and assign an assistant. 
+  `;
+
   return (
     <CustomModal
       trigger={null}
       title="No valid assistant for this teamspace"
       open={!assistants || assistants.length === 0}
-      description="There is currently no valid assistant assigned to this teamspace, so
-          the chat page cannot be used. This may be because the teamspace is
-          still syncing, or an assistant has not been assigned yet."
+      description={isTeamspaceAdmin ? adminDescription : basicUserDescription}
     >
       <div className="flex gap-2 justify-end">
         <Link href="/chat" passHref>
