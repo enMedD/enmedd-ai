fcdd075c9f352ef6aca1940d8a5e08eac0113105
SchadenKai <kairustecsonpersonal@gmail.com>
2025-01-02 03:18:44 +0800

    fix: update brand color definitions to use CSS variables in tailwind config

--- web/tailwind-themes/tailwind.config.ts
+++ web/tailwind-themes/tailwind.config.ts
@@ -44,17 +44,17 @@ const config = {
         foreground: "hsl(var(--foreground))",
         // TODO: make a proper palette for this that uses range
         brand: {
-          50: "#E6E6E6",
-          100: "#CCCCCC",
-          200: "#999999",
-          300: "#666666",
-          400: "#333333",
-          500: "#000000",
-          600: "#000000",
-          700: "#000000",
-          800: "#000000",
-          900: "#000000",
-          950: "#000000",
+          50: "var(--brand-50)",
+          100: "var(--brand-100)",
+          200: "var(--brand-200)",
+          300: "var(--brand-300)",
+          400: "var(--brand-400)",
+          500: "var(--brand-500)",
+          600: "var(--brand-600)",
+          700: "var(--brand-700)",
+          800: "var(--brand-800)",
+          900: "var(--brand-900)",
+          950: "var(--brand-950)",
         },
 
         secondary: {
